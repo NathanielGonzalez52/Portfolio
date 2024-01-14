8987247100fe1fbf04274c727aea7406331a6cb6
Nathan Gonzalez <nathanielgonzalez52@gmail.com>
2024-01-13 21:16:16 -0800

    Delete separate projects

--- app/welcome.js
+++ app/welcome.js
@@ -19,14 +19,10 @@ export default function welcome() {
     <>
     <div className="landing">
       <Type/>
-      <Fade delay={7000}>
-        <a href="../my-app/SenorTaco-2-main/index.js" className="test">test</a>
+      <Fade delay={7000} duration={2000}>
         <button className="projects-btn">Projects</button>
       </Fade>
-      {/* <img alt="Missing" src="../images/ng.png"></img> */}
-      <img alt="Missing" src="../SenorTaco-2-main/images_st/1.png"></img>
-
-    </div>
+         </div>
     
     </>
   )
