9c1e0462c7f9c2085660dfd81962c8350209d8a7
Nathan Gonzalez <nathanielgonzalez52@gmail.com>
2024-01-15 10:55:39 -0800

    Modified about page. Add pictures to image folder

---
+++ app/contact/page.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./page.css";
+import Navbar from "../navbar";
+
+
+export default function contact() {
+  return (
+    <>
+    <Navbar />
+    <div className="contact">
+        <div className="envelope">
+            <img className="mail" src="../../images/mail.png"></img>
+        </div>
+        <h1>Contact Me</h1>
+        <h3 className="detail">I would love to hear about your dream website. Let's connect and build something together.</h3>
+        <form className="contact-me block">
+            <label htmlFor="frm-email"> 
+                <input
+                id="frm-email"
+                placeholder="Your Email Address"
+                type="email"
+                name="email"
+                autoComplete="email"
+                required
+                />
+            </label>
+            {/* <div className="connect"> */}
+                <label className="connect"></label>
+                <textarea 
+                id="freeform" 
+                name="freeform" 
+                rows="14" 
+                cols="60"
+                placeholder="Please tell me a bit about yourself or the organization you represent"></textarea>
+                <input 
+                className="send" 
+                type="submit" 
+                value="Submit"></input>
+            {/* </div> */}
+        </form>
+      </div>
+      </>
+  )
+}
+
+{/* <a href="https://www.flaticon.com/free-icons/contact" title="contact icons">Contact icons created by Freepik - Flaticon</a> */}
